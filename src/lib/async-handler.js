180e7fcaf25976c7e4fdb426b80c5e85b4d60a48
Don Henton <donhenton@gmail.com>
2019-10-04 13:10:17 -0500

    clean up

--- src/lib/async-handler.js
+++ src/lib/async-handler.js
@@ -1,6 +1,3 @@
-// const asyncHandler = fn => (req, res, next) => {
-//     Promise.resolve(fn(req, res, next)).catch(err => next(err));
-//   };
 
 //https://itnext.io/using-async-await-to-write-cleaner-route-handlers-7fc1d91b220b
 
